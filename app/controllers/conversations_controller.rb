5c88faaba78291d8bb3f504e1109275863542983
Thomas Gee <thomasrobertgee@gmail.com>
2016-11-23 16:07:15 +1100

    completed internal messaging system

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,5 +1,5 @@
 class ConversationsController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user!
 
   def index
     @users = User.all
@@ -22,5 +22,5 @@ class ConversationsController < ApplicationController
   def conversation_params
     params.permit(:sender_id, :recipient_id)
   end
-  
+
 end
