6c7a42574b78bd7b4abe4c9dc2e44784386ebdba
Thomas Gee <thomasrobertgee@gmail.com>
2016-11-23 15:03:53 +1100

    mid-way through messages integration - beginning testing and bug squashing

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123002825) do
+ActiveRecord::Schema.define(version: 20161123032803) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "listings", force: :cascade do |t|
@@ -30,6 +37,17 @@ ActiveRecord::Schema.define(version: 20161123002825) do
     t.index ["category_id"], name: "index_listings_on_category_id"
     t.index ["photo_id"], name: "index_listings_on_photo_id"
     t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|
